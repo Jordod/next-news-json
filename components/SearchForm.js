01cdb0e1dad545fc35d46f494fcbf20e73fd97de
Jordan <jordodsbot@gmail.com>
2018-11-20 21:27:55 +0000

    Added Categories

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -6,39 +6,63 @@ import Select from 'react-select'
 export default class SearchForm extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
-            selected : null,
-            options : []
+            selected: null,
+            selectedCat: null,
+            options: [],
+            categories: []
         };
     }
 
+
     handleChange = (option) => {
-        this.setState({ option });
+        this.setState({ selected: option });
         this.props.setNewsSource(option);
         event.preventDefault();
     }
 
+    handleCategory = (option) => {
+        this.setState({ selectedCat: option });
+        this.props.setNewsCategory(option);
+        event.preventDefault();
+    }
+
     getOptions = () => {
-        for (let i = 0; i < this.props.sources.length; i++) {
-            this.state.options.push({value : this.props.sources[i].id, label : this.props.sources[i].name});
+        if (this.state.options.length == 0) {
+            for (let i = 0; i < this.props.sources.length; i++) {
+                this.state.options.push({ value: this.props.sources[i].id, label: this.props.sources[i].name });
+            }
+        }
+        if (this.state.categories.length == 0) {
+            for (let i = 0; i < this.props.categories.length; i++) {
+                this.state.categories.push({ value: this.props.categories[i], label: this.props.categories[i] });
+            }
         }
     }
 
     // Render the form
     render() {
-        const { selected } = this.state;
         this.getOptions();
+        const { selected } = this.state;
+        const { selectedCat } = this.state;
 
         return (
             <div>
                 <div id="search">
                     <h3>Select newsapi.org source</h3>
-                    <Select 
+                    <Select
                         value={selected}
                         onChange={this.handleChange}
                         options={this.state.options}
                         placeholder={"Select or search for News Source.."}
+                    />
+
+                    <Select
+                        value={selectedCat}
+                        onChange={this.handleCategory}
+                        options={this.state.categories}
+                        placeholder={"Select by Category.."}
                     />
                 </div>
                 <style jsx>{`
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,151 @@
 // This is the Link API
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import SearchForm from '../components/SearchForm';
 
-// Pass this content as 'props' to child components
-const Index = props => (
-  <div>
-  <h1>Batman TV Shows</h1>
-  <ul>
-    {props.shows.map(({show}) => (
-      <li key={show.id}>
-        <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-          <a>{show.name}</a>
-        </Link>
-      </li>
-    ))}
-  </ul>
-  </div>
-);
 
-// see https://nextjs.org/learn/basics/fetching-data-for-pages
+export default class News extends React.Component {
 
-Index.getInitialProps = async function() {
+  constructor(props) {
+    super(props)
 
-  const url = 'https://api.tvmaze.com/search/shows?q=batman';
+    this.state = {
+      newsSource: "the-irish-times",
+      newsSourceName: "The Irish Times",
+      articles: [],
+      category : null
+    }
+  }
 
-  const res = await fetch(url)
-  const data = await res.json()
+  setNewsSource = input => {
+    this.setState({
+      newsSource: input.value,
+      newsSourceName : input.label,
+    })
+  }
 
-  console.log(`Show data fetched. Count: ${data.length}`)
+  setNewsCategory = input => {
+    this.setState({
+      category : input.value
+    })
+  }
 
-  return {
-    shows: data
+  // Method to render the page
+  render() {
+    // If state.articles is empty then copy value from props
+    if (this.state.articles.length == 0) {
+      this.state.articles = this.props.articles;
+    }
+
+    // Return the page content
+    return (
+      <div>
+        <div style={{maxWidth : "60%"}}>
+          <SearchForm setNewsSource={this.setNewsSource} setNewsCategory={this.setNewsCategory} sources={this.props.sources} categories={this.props.categories}/>
+        </div>
+        <div>
+          <h3>News from {!(this.state.newsSourceName == null) ? <span>{this.state.newsSourceName}</span> : <span>{this.state.category}</span>}</h3>
+          {this.state.articles.map((article, index) => (
+            <section key={index}>
+              <h3>{article.title}</h3>
+              <p className="author">{article.author}&nbsp;&nbsp;-&nbsp;&nbsp;{new Date(article.publishedAt).toLocaleString()}</p>
+              {!article.urlToImage ? <p></p> : <img src={article.urlToImage} alt="article image" className="article-img"></img>}
+              <p>{article.description}</p>
+              <p>{article.content}</p>
+              <p><Link href={article.url}><a>Read More</a></Link></p>
+            </section>
+          ))}
+        </div>
+
+        <style jsx>{`
+            section {
+                width: 60%;
+                min-width: 540px;
+                border: 1px solid gray;
+                background-color: rgb(32,32,32);
+                padding: 1em;
+                margin: 1em;
+                box-shadow: 5px 5px 5px #333333;
+                color: rgb(239,239,239);
+            }
+
+            h2, h3 {
+                font-family: "Arial";
+                color: rgb(239,239,239);
+                padding: 5px;
+            }
+
+            a {
+                text-decoration: none;
+            }
+
+            a:hover {
+                font-weight: bold;
+            }
+
+            a:link {
+                color: #fcce5a;
+            }
+
+            a:visited {
+                color: white;
+            }
+
+            .date {
+                font-style: italic;
+                font-size: 0.8em;
+            }
+
+            .article-img {
+                max-width: 50%;
+            }
+        `}</style>
+
+      </div>
+
+    );
+
+  }
+  static async getInitialProps() {
+    const newsSource = "the-irish-times";
+    const apiKey = "751db7e54ff74e09a364481e79fb466d";
+    const url = `https://newsapi.org/v2/top-headlines?sources=${newsSource}&apiKey=${apiKey}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const sourceUrl = `https://newsapi.org/v2/sources?&apiKey=${apiKey}`;
+    const sourceRes = await fetch(sourceUrl);
+    const sourceData = await sourceRes.json();
+
+    return {
+      articles: data.articles,
+      sources : sourceData.sources,
+      categories : ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+    }
+  }
+
+
+  async componentDidUpdate(prevProps, prevState) {
+
+    if (this.state.newsSource !== prevState.newsSource) {
+      const apiKey = '751db7e54ff74e09a364481e79fb466d';
+      const url = `https://newsapi.org/v2/top-headlines?sources=${this.state.newsSource}&apiKey=${apiKey}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      this.state.articles = data.articles;
+      this.setState(this.state);
+    }
+
+    if (this.state.category !== prevState.category) {
+      const apiKey = '751db7e54ff74e09a364481e79fb466d';
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.state.category}&apiKey=${apiKey}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      this.state.articles = data.articles;
+      this.state.newsSourceName = null;
+      this.setState(this.state);
+    }
+
   }
 }
-
-export default Index
